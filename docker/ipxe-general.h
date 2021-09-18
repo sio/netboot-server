e9097347e19e55c66ccb45926dd764833195ce9f
Vitaly Potyarkin <sio.wtf@gmail.com>
2021-09-18 22:18:39 +0300

    Use same set of commands as netboot.xyz
    Chainbooting fails otherwise

--- docker/ipxe-general.h
+++ docker/ipxe-general.h
@@ -9,3 +9,15 @@
 
 #define NSLOOKUP_CMD
 #define PING_CMD
+
+/* Compatibility with netboot.xyz */
+#define CONSOLE_CMD           /* Console command */
+#define DIGEST_CMD            /* Image crypto digest commands */
+#define IMAGE_TRUST_CMD       /* Image trust management commands */
+#define NTP_CMD               /* NTP commands */
+#define PARAM_CMD             /* Form parameter commands */
+#define PCI_CMD               /* PCI commands */
+#define POWEROFF_CMD          /* Power off commands */
+#define REBOOT_CMD            /* Reboot command */
+#define TIME_CMD              /* Time commands */
+#define VLAN_CMD              /* VLAN commands */
